743270c17f7ba85a50acbafb6d383374319ed97d
LuccashAlves <74920252+LuccashAlves@users.noreply.github.com>
2022-11-23 22:18:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10000000)
 
 
 from sortFunctions.bubbleSort import getInfoBubbleSort
@@ -10,21 +10,15 @@ from sortFunctions.mergeSort import getInfoMergeSort
 from sortFunctions.quickSort import getInfoQuickSort
 from sortFunctions.selectionSort import getInfoSelectionSort
 
-#from sortFunctions.bubbleSort import bubbleSort, getInfoBubbleSort
 import sortTools as tools
-#from sortFunctions.insertionSort import insertionSort, getInfoInsertionSort
-#from sortFunctions.mergeSort import getInfoMergeSort
-#from sortFunctions.selectionSort import getInfoSelectionSort
 
-#Lista de 1000
-tam = 10000
 
-#getInfoBubbleSort(tam)
-#getInfoInsertionSort(tam)
-#getInfoSelectionSort(tam)
-#getInfoMergeSort(tam)
+#Elementos da Lista
+tam = 1000
+
+getInfoBubbleSort(tam)
+getInfoInsertionSort(tam)
+getInfoSelectionSort(tam)
+getInfoMergeSort(tam)
 getInfoQuickSort(tam)
-#getInfoImprovedBubbleSort(tam)
-
-
-#print("=QuickSort     ", sorts.quickSort(listSelected,0,size-1))
+getInfoImprovedBubbleSort(tam)
